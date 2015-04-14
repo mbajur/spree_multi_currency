bf0ecf15c06fc92a24d310e6b644f8ec969b5779
Mike Bajur <mbajur@gmail.com>
2015-04-14 23:29:25 +0200

    Hound improvements

--- app/assets/javascripts/spree/backend/spree_multi_currency.js
+++ app/assets/javascripts/spree/backend/spree_multi_currency.js
@@ -1,38 +1,40 @@
 //= require spree/backend
 
 $(document).ready(function(){
-	// custom css expression for a case-insensitive contains()
   $.expr[':'].Contains = function(a,i,m){
-      return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+    return (a.textContent || a.innerText || '').toUpperCase()
+    																					 .indexOf(
+    																					 		m[3].toUpperCase()
+    																					 	) >= 0;
   };
 
 
-  function listFilter(list) { // header is any element, list is an unordered list
-    // create and add the filter form to the header
-    var input = $("#variant-price-search");
+  function listFilter(list) {
+    var input = $('#variant-price-search');
 
     $(input)
       .change( function () {
         var filter = $(this).val();
         if(filter) {
-          // this finds all links in a list that contain the input,
-          // and hide the ones not containing the input while showing the ones that do
-          $(list).find(".panel-title:not(:Contains(" + filter + "))").parent().parent().parent().hide();
-          $(list).find(".panel-title:Contains(" + filter + ")").parent().parent().parent().show();
+          $(list).find('.panel-title:not(:Contains(' + filter + '))')
+          			 .parent().parent().parent()
+          			 .hide();
+          $(list).find('.panel-title:Contains(' + filter + ')')
+          			 .parent().parent().parent()
+          			 .show();
         } else {
-          $(list).find(".panel").parent().show();
+          $(list).find('.panel').parent().show();
         }
         return false;
       })
     .keyup( function () {
-        // fire the above change event after every letter
-        $(this).change();
+      $(this).change();
     });
   }
 
 
   //ondomready
   $(function () {
-    listFilter($("#variant-prices"));
+    listFilter($('#variant-prices'));
   });
-})
+});
